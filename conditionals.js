a8a09c3a3088111e37a27b17012e755e9822931d
7777-Katib <85574228+7777-Katib@users.noreply.github.com>
2021-12-04 13:41:46 +0600

    Add files via upload

---
+++ conditionals.js
@@ -0,0 +1,27 @@
+
+var biscuitPrice=8
+if(biscuitPrice<10){
+     console.log("I will eat biscuits");
+}
+else{
+    console.log("tea is better");
+
+}
+
+
+var jobPaiso=true;
+var savingAmount=5000;
+
+if(jobPaiso==false || savingAmount>20000){
+    console.log("aso kheli");
+
+}
+
+else if (jobPaiso == false){
+    console.log("bingo");
+}
+
+else{
+    console.log("enjoy urself");
+}
+
