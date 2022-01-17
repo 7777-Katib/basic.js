1e2a97f72fd06c8c7cc933c5d15b0157e52bd5f6
7777-Katib <85574228+7777-Katib@users.noreply.github.com>
2022-01-17 20:13:05 +0600

    Add files via upload

---
+++ bonus.js
@@ -0,0 +1,13 @@
+var mark=[30,40,450,50,70,80,90];
+
+for (var i=0;i<mark.length;i++){
+    var element=mark[i];
+    console.log(element);
+}
+
+console.log("outside");
+console.log("alkatra");
+
+
+
+
